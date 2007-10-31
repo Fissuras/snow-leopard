21e916ca1ca801142235276a69a165c3b7481a52
LeegleechN <LeegleechN@9f4c51f2-c03d-0410-95bf-fdd4e76fd4f3>
2007-10-31 14:33:35 +0000

    Added faction, displaySize,sourceSize, and displayOffset properties to the GameObject class.

--- trunk/Snow-Leopard/Snow-Leopard/GameObject.cpp
+++ trunk/Snow-Leopard/Snow-Leopard/GameObject.cpp
@@ -4,8 +4,15 @@ class GameObject
 {
 	string displayName; 
 	private float velocity [2];
-	int displayID;
+	int faction;
+	int displaySize;
 	int[2] location;
+
+	int displayID;
+	int sourceSize;
+	int displayOffset; // if there are multiple display frames in one bitmap, choose which one the GameObject has
+
+	
 
 	bool doActions(WorldState* worldState)
 	{
