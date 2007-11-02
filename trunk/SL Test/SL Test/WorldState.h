df63fa049c6b0adbe4c6974fffe51a97f14f40f7
LeegleechN <LeegleechN@9f4c51f2-c03d-0410-95bf-fdd4e76fd4f3>
2007-11-02 02:46:12 +0000

    Added 2d matrix class (the way we were doing it before doesn't work in C++) Attempted to flesh out WorldState class, but couldn't get it working right

--- trunk/SL Test/SL Test/WorldState.h
+++ trunk/SL Test/SL Test/WorldState.h
@@ -4,24 +4,25 @@
 #include <list>
 
 #include "GameObject.h"
+#include "Matrix.h"
 
 class GameObject;
 
-typedef std::list<GameObject*>** MatrixType;
 typedef std::list<GameObject*> GameObjectList;
 
 class WorldState
 {
 public:
 
+WorldState::WorldState();
 bool insertObject(GameObject* gameObject, int location[2]);
 bool deleteObject(GameObject* gameObject);
 bool moveObject(GameObject* gameObject, int location[2]);
-bool getEnvironment(int location[2], int size, MatrixType* memory);
+bool getEnvironment(int location[2], int size, Matrix<GameObjectList>* memory);
 GameObjectList getAtLocation(int[2]);
 
 private:
-	MatrixType worldMatrix; //2d array of gameobjects. (each x,y holds a list)
+	Matrix<GameObjectList> *worldMatrix; //2d array of gameobjects. (each x,y holds a list)
 };
 
 #endif
