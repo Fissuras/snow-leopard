3011c34966d17b0ced49a95020ebf55d9c5b35f8
LeegleechN <LeegleechN@9f4c51f2-c03d-0410-95bf-fdd4e76fd4f3>
2007-11-04 21:04:17 +0000

    added point.cpp and point.h slight progress on renderer

--- trunk/Snow Leopard/Snow Leopard/Renderer.cpp
+++ trunk/Snow Leopard/Snow Leopard/Renderer.cpp
@@ -3,32 +3,48 @@
 #include <SDL.h>
 #include <SDL_TTF.h>
 #include "Renderer.h"
+#include <iostream>
 
 
-bool Renderer::Render(WorldState* worldState)
+bool Renderer::Render(WorldState* state)
 {
+	for (int x = 0;x<state->xsize;x++)
+	{
+		for (int y = 0;y<state->ysize;y++)
+		{
+			GameObjectList* list = state->getAtLocation(new point(x,y));
+			GameObjectIter itr;
+			for (itr = list->begin();itr!=list->end();itr++)
+			{
+				GameObject* obj = *itr;
+				std::cout << obj->displayName;
+			}
+		}
+	}
 	return true;
 }
 
 bool Renderer::init()
 {
-	try
-	{
-	SDL_Init(SDL_INIT_EVERYTHING);
-	TTF_Init();
-    atexit(TTF_Quit);
-	atexit(SDL_Quit);
+	//try
+	//{
+	//SDL_Init(SDL_INIT_EVERYTHING);
+	//TTF_Init();
+ //   atexit(TTF_Quit);
+	//atexit(SDL_Quit);
 
-	SDL_WM_SetCaption( "Snow Leopard", NULL );
-    
-	SDL_Surface *screen = NULL;
-    screen = SDL_SetVideoMode( 0, 0, 0, SDL_FULLSCREEN | SDL_HWSURFACE | SDL_DOUBLEBUF); //0 means current mode
-	int screenWidth = SDL_GetVideoInfo()->current_w;
-	int screenHeight = SDL_GetVideoInfo()->current_h;
+	//SDL_WM_SetCaption( "Snow Leopard", NULL );
+ //   
+	//SDL_Surface *screen = NULL;
+ //   screen = SDL_SetVideoMode( 0, 0, 0, SDL_FULLSCREEN | SDL_HWSURFACE | SDL_DOUBLEBUF); //0 means current mode
+	//int screenWidth = SDL_GetVideoInfo()->current_w;
+	//int screenHeight = SDL_GetVideoInfo()->current_h;
+	//return true;
+	//}
+	//catch(...)
+	//{
+	//	return false;
+	//}
+	
 	return true;
-	}
-	catch(...)
-	{
-		return false;
-	}
 }
