2b245a5af8235024ff2cd80a2f31b1b554a7a2e3
LeegleechN <LeegleechN@9f4c51f2-c03d-0410-95bf-fdd4e76fd4f3>
2007-11-10 06:56:46 +0000

    Added a folder remotely

---
+++ trunk/Snow Leopard/Snow Leopard/Snow Leopard/Ship.cpp
@@ -0,0 +1,27 @@
+#include "Ship.h"
+
+
+bool Ship::doActions(WorldState* worldState)
+{
+
+	move(worldState);
+	shoot(worldState);
+	return true;
+	
+}
+
+bool Ship::move(WorldState* worldState)
+{
+	return true;
+}
+
+bool Ship::shoot(WorldState* worldState)
+{
+	return true;
+}
+
+Ship::Ship(std::string name, int fact, point *p) : GameObject(name,fact,p)
+{
+	actionPriority = ShipPriority;
+}
+	
