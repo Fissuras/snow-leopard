f135cf272db0505b2b9c1946316d77358ff3dd90
LeegleechN <LeegleechN@9f4c51f2-c03d-0410-95bf-fdd4e76fd4f3>
2007-11-14 05:45:30 +0000

    Implemented "ShotgunCannon"

---
+++ Snow Leopard/Test/ShotgunCannon.h
@@ -0,0 +1,29 @@
+#ifndef SHOTGUNCANNON_H_
+#define SHOTGUNCANNON_H_
+#include "WorldState.h"
+#include "SevnaMark40.h"
+
+class ShotgunCannon:public Weapon
+{
+	public:
+		static const int coolDownInterval = 600;
+		void fire(WorldState* state, point location, double heading, double speed, CL_ResourceManager* resources)
+		{
+			for (int i=0;i<5;i++)
+			{
+			SevnaMark40* p = new SevnaMark40("SevnaMark40",resources);
+			p->resourceName = "SevnaMark40";
+			p->heading = heading - 24 + 6*i;
+			p->speed = speed + 2;
+			state->insertObject(p,location.offsetPolar(heading,2));
+			}
+			
+			Weapon::timeLastFired = state->time;
+		};
+		ShotgunCannon::ShotgunCannon()
+		{
+			Weapon::timeLastFired = 0;
+		};
+};
+
+#endif
--- Snow Leopard/Test/Starfury.cpp
+++ Snow Leopard/Test/Starfury.cpp
@@ -4,6 +4,7 @@
 Starfury::Starfury(std::string resourceName,CL_ResourceManager* resources):Ship(resourceName,resources)
 {
 	pulseCannon = new PulseCannon();
+	shotgunCannon = new ShotgunCannon();
 }
 bool Starfury::move()
 {
@@ -16,9 +17,14 @@ bool Starfury::shoot()
 {
 	int time = GameObject::worldState->time;
 
-	if ((pulseCannon->timeLastFired==0) || (pulseCannon->timeLastFired + pulseCannon->coolDownInterval <= GameObject::worldState->time))
+	/*if ((pulseCannon->timeLastFired==0) || (pulseCannon->timeLastFired + pulseCannon->coolDownInterval <= GameObject::worldState->time))
 	{
 		pulseCannon->fire(worldState,location,heading,speed,GameObject::resources);
+	}*/
+
+	if ((shotgunCannon->timeLastFired==0) || (shotgunCannon->timeLastFired + shotgunCannon->coolDownInterval <= GameObject::worldState->time))
+	{
+		shotgunCannon->fire(worldState,location,heading,speed,GameObject::resources);
 	}
 	return true;
 }
